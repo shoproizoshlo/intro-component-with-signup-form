fdbddfa65cce9bf68f3b1e03bf0b9b0efe2cd8ed
shoproizoshlo <iamsusunny@gmail.com>
2023-09-14 12:38:51 +0200

    cleaned some files

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,70 +1,90 @@
-const form = document.getElementById("form");
-const firstName = document.getElementById("firstName");
-const lastName = document.getElementById("lastName");
-const email = document.getElementById("email");
-const password = document.getElementById("password");
+// creating function that get form selector
+const validateForm = (formSelector) => {
+  const formElement = document.querySelector(formSelector);
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
+  // items in array have object and 3 properties
+  const validationOptions = [
+    // check if value pass minlength
+    {
+      attribute: "minlength",
+      isValid: (input) =>
+        input.value && input.value.length >= parseInt(input.minLength),
+      errorMessage: (input, label) =>
+        `${label.textContent} needs to be at least ${input.minLength} characters`,
+    },
+    // check if value pass email
+    {
+      attribute: "pattern",
+      isValid: (input) => {
+        const patternRegex = new RegExp(input.pattern);
+        return patternRegex.test(input.value);
+      },
+      errorMessage: (input, label) => `Looks like this is not an email`,
+    },
 
-  validateInputs();
-});
+    // check if value pass required
+    {
+      attribute: "required",
+      isValid: (input) => input.value.trim() !== "",
+      errorMessage: (input, label) => `${label.textContent} cannot be empty`,
+    },
+  ];
 
-const setError = (element, message) => {
-  const inputControl = element.parentElement;
-  const errorDisplay = inputControl.querySelector(".error");
+  const validateSingleFormGroup = (formGroup) => {
+    // select input, label, error img and error class
+    const input = formGroup.querySelector("input, textarea");
+    const label = formGroup.querySelector("label");
+    const errorContainer = formGroup.querySelector(".error");
+    const errorIcon = formGroup.querySelector(".error-icon");
 
-  errorDisplay.innerText = message;
-  inputControl.classList.add("error");
-  inputControl.classList.remove("success");
+    //   set no error to the input
+    let formGroupError = false;
+    // validation rules that loop through then check the input egainst each of rules and trigger right error message
+    for (const option of validationOptions) {
+      if (input.hasAttribute(option.attribute) && !option.isValid(input)) {
+        errorContainer.textContent = option.errorMessage(input, label);
+        errorContainer.style.display = "block";
+        //   add red border around input
+        input.classList.add("border-danger-subtle");
+        input.style.margin = "0";
+        //   add error icon into input
+        errorIcon.classList.remove("hidden");
+        //   add error to the input
+        formGroupError = true;
+      }
+    }
+    //   remove error border and icon from input if here is no error
+    if (!formGroupError) {
+      input.classList.remove("border-danger-subtle");
+      errorIcon.classList.add("hidden");
+      errorContainer.textContent = "";
+    }
+  };
+
+  // disabling HTML standart validation
+  formElement.setAttribute("novalidate", "");
+
+  //adding our validation
+  formElement.addEventListener("submit", (event) => {
+    // prevent page reload after submit
+    event.preventDefault();
+    // go to validation all inputs
+    validateAllFormGroups(formElement);
+  });
+
+  // validate all inputs
+  const validateAllFormGroups = (formToValidate) => {
+    //   create array from all inputs
+    const formGroups = Array.from(
+      formToValidate.querySelectorAll(".formGroup")
+    );
+
+    //   loop through array
+    formGroups.forEach((formGroup) => {
+      validateSingleFormGroup(formGroup);
+    });
+  };
 };
 
-const setSuccess = (element) => {
-  const inputControl = element.parentElement;
-  const errorDisplay = inputControl.querySelector(".error");
-
-  errorDisplay.innerText = "";
-  inputControl.classList.add("success");
-  inputControl.classList.remove("error");
-};
-
-const isValidEmail = (email) => {
-  const re =
-    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
-};
-
-const validateInputs = () => {
-  const firstNameValue = firstName.value.trim();
-  const lastNameValue = lastName.value.trim();
-  const emailValue = email.value.trim();
-  const passwordValue = password.value.trim();
-
-  if (firstNameValue === "") {
-    setError(firstName, "First Name cannot be empty");
-  } else {
-    setSuccess(firstName);
-  }
-
-  if (lastNameValue === "") {
-    setError(lastName, "Last Name cannot be empty");
-  } else {
-    setSuccess(lastName);
-  }
-
-  if (emailValue === "") {
-    setError(email, "Email cannot be empty");
-  } else if (!isValidEmail(emailValue)) {
-    setError(email, "Looks like this is not an email");
-  } else {
-    setSuccess(email);
-  }
-
-  if (passwordValue === "") {
-    setError(password, "Password cannot be empty");
-  } else if (passwordValue.length < 8) {
-    setError(password, "Password must be at least 8 character");
-  } else {
-    setSuccess(password);
-  }
-};
+// select form id
+validateForm("#registrationForm");
